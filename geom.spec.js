5e2421a126c950d763a63461f53f0178d283a7d5
Melissa Miller <lis.d.miller@gmail.com>
2016-02-28 10:40:45 -0500

    Adds instructions for using tests

--- geom.spec.js
+++ geom.spec.js
@@ -1,3 +1,17 @@
+// Save this file into your js_geometry directory.
+//
+// Execute the tests with:
+//
+// ```bash
+// $ jasmine-node .
+// ```
+//
+// All but the first test have been skipped.
+//
+// Once you get a test passing, you can unskip the next one by
+// changing `xit` to `it`.
+
+
 var Geom = require('./geom');
 var Rectangle = Geom.Rectangle;
 var Triangle = Geom.Triangle;
@@ -10,17 +24,17 @@ describe('Rectangle', function() {
     expect(rectangle.isSquare()).toBe(true);
   });
 
-  it('checks for non squares', function() {
+  xit('checks for non squares', function() {
     var rectangle = new Rectangle(3, 4);
     expect(rectangle.isSquare()).toBe(false);
   });
 
-  it('calculates area', function() {
+  xit('calculates area', function() {
     var rectangle = new Rectangle(3,3);
     expect(rectangle.area()).toEqual(9);
   });
 
-  it('finds the perimeter', function() {
+  xit('finds the perimeter', function() {
     var rectangle = new Rectangle(2, 3);
     expect(rectangle.perimeter()).toEqual(10);
   });
@@ -28,44 +42,44 @@ describe('Rectangle', function() {
 
 describe('Triangle', function() {
 
-  it('checks for equilaterals', function() {
+  xit('checks for equilaterals', function() {
     var triangle = new Triangle(2, 2, 2);
     expect(triangle.isEquilateral()).toBe(true);
   });
 
-  it('checks for non equilaterals', function() {
+  xit('checks for non equilaterals', function() {
     var triangle = new Triangle(3, 4, 5);
     expect(triangle.isEquilateral()).toBe(false);
   });
 
-  it('checks for isosceles', function() {
+  xit('checks for isosceles', function() {
     var triangle = new Triangle(3, 5, 3);
     expect(triangle.isIsosceles()).toBe(true);
   });
 
-  it('checks for non isosceles', function() {
+  xit('checks for non isosceles', function() {
     var triangle = new Triangle(1, 2, 3);
     expect(triangle.isIsosceles()).toBe(false);
   });
 
-  it('calculates the area', function() {
+  xit('calculates the area', function() {
     var triangle = new Triangle(4, 5, 3);
     expect(triangle.area()).toEqual(6);
   });
 
-  it('checks for obtuse', function() {
+  xit('checks for obtuse', function() {
     var triangle = new Triangle(2, 3, 4);
     expect(triangle.isObtuse()).toBe(true);
   });
 
-  it('checks for non obtuse', function() {
+  xit('checks for non obtuse', function() {
     var triangle = new Triangle(9, 40, 41);
     expect(triangle.isObtuse()).toBe(false);
   });
 });
 
 describe('LineSegment', function() {
-  it('calculates length of a line', function() {
+  xit('calculates length of a line', function() {
     var line = new LineSegment(2,1, 6,4);
     expect(line.length()).toEqual(5);
   });
